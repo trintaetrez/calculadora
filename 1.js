3e16bfd1ddecb13b4909fca581058f9a40fc3334
Fernando Campos da Cruz <fernandocdacruz@gmail.com>
2024-03-31 23:02:40 -0300

    3º commit - mais alteracoes

--- 1.js
+++ 1.js
@@ -1,9 +1,41 @@
-const calculator = document.querySelector('calculator');
-const keys = calculator.querySelector('calculator__keys');
+//31-03-2024 Quando um usuário aperta a tecla do sinal de igual
+
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         const key = e.target;
-        const action = key.dataset.action
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
+        }
+
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.';
+        }
+
+        if (action === 'clear') {
+            console.log('clear key!');
+        }
+
+        if (action === 'calculate') {
+            console.log('equal key!');
+        }
+
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
     }
 })
